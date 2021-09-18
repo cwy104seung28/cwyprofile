819b2c403669b90779e0b23904a4256434f3e0a0
cwyADT107102 <68813383+cwy104seung28@users.noreply.github.com>
2021-09-19 03:12:24 +0800

    0919 add require

--- js/index.js
+++ js/index.js
@@ -1,11 +1,2 @@
-$(".paroller, [data-paroller-factor]").paroller({
-    factor: 0.3,            // multiplier for scrolling speed and offset
-    factorXs: 0.1,          // multiplier for scrolling speed and offset if window width is <576px
-    factorSm: 0.2,          // multiplier for scrolling speed and offset if window width is <=768px
-    factorMd: 0.2,          // multiplier for scrolling speed and offset if window width is <=1024px
-    factorLg: 0.3,          // multiplier for scrolling speed and offset if window width is <=1200px
-    type: 'foreground',     // background, foreground
-    direction: 'horizontal' ,// vertical, horizontal
-    transition: 'transform 0.1s ease' // CSS transition, added on elements where type:'foreground'
-});
-$("[data-paroller-factor]").paroller();
+require('paroller.js');
+require("jquery");
